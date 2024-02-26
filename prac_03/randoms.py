0ef6e9c35060e455d6c95a8797416f75893be5d9
Artyom <artyom.axyutits@my.jcu.edu.au>
2024-02-26 17:22:06 +0800

    Make the whole program from scratch

---
+++ prac_03/randoms.py
@@ -0,0 +1,5 @@
+import random
+
+print(random.randint(5, 20))  # line 1. Gives random number 5 to 20
+print(random.randrange(3, 10, 2))  # line 2. Gives random number from 3 to 10 with a step 2, i.e. 3,,5,7,9
+print(random.uniform(2.5, 5.5))  # line 3. Gives random float number from 2.5 to 5.5
